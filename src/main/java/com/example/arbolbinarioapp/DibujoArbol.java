c536e33765eae3c3b18ccc16d2064f09eff0b542
Bou <mariac.aranzazur@uqvirtual.edu.co>
2024-10-27 20:58:16 -0500

    comentarios

--- src/main/java/com/example/arbolbinarioapp/DibujoArbol.java
+++ src/main/java/com/example/arbolbinarioapp/DibujoArbol.java
@@ -9,16 +9,16 @@ public class DibujoArbol extends Canvas {
 
     public DibujoArbol(ABB arbol) {
         this.arbol = arbol;
-        this.setWidth(1000);  // Anchura ajustada para visualizar el árbol horizontalmente
-        this.setHeight(800);  // Altura suficiente para espacio vertical entre niveles
+        this.setWidth(1000);
+        this.setHeight(800);
     }
 
     public void dibujarArbol() {
         GraphicsContext gc = getGraphicsContext2D();
         gc.clearRect(0, 0, getWidth(), getHeight());
         if (arbol.getRaiz() != null) {
-            // Dibuja la raíz un poco a la izquierda de la mitad de la ventana
-            dibujarNodo(gc, arbol.getRaiz(), 30, getHeight() / 2, 70); // x inicial y altura media
+
+            dibujarNodo(gc, arbol.getRaiz(), 30, getHeight() / 2, 70);
         }
     }
 
@@ -27,21 +27,21 @@ public class DibujoArbol extends Canvas {
 
         Color colorNodo = obtenerColorNodo(nodo);
         gc.setFill(colorNodo);
-        gc.fillOval(x - 15, y - 15, 30, 30); // Dibuja el nodo como un círculo
+        gc.fillOval(x - 15, y - 15, 30, 30);
         gc.setFill(Color.BLACK);
-        gc.fillText(String.valueOf(nodo.getValor()), x - 5, y + 5); // Escribe el valor del nodo
+        gc.fillText(String.valueOf(nodo.getValor()), x - 5, y + 5);
 
-        double nuevoXOffset = 80; // Desplazamiento horizontal para cada nodo
-        double nuevoYOffset = yOffset * 0.7; // Ajuste vertical entre niveles
+        double nuevoXOffset = 80;
+        double nuevoYOffset = yOffset * 0.7;
 
         if (nodo.getDerecho() != null) {
-            // Nodo mayor hacia arriba
+
             gc.strokeLine(x, y, x + nuevoXOffset, y - nuevoYOffset);
             dibujarNodo(gc, nodo.getDerecho(), x + nuevoXOffset, y - nuevoYOffset, nuevoYOffset);
         }
 
         if (nodo.getIzquierdo() != null) {
-            // Nodo menor hacia abajo
+          
             gc.strokeLine(x, y, x + nuevoXOffset, y + nuevoYOffset);
             dibujarNodo(gc, nodo.getIzquierdo(), x + nuevoXOffset, y + nuevoYOffset, nuevoYOffset);
         }
